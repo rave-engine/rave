dc6279f907fd29de0777303e2af04a9be3dc3e52
Shiz <hi@shiz.me>
2014-01-18 21:15:05 +0100

    rave: Update __init__.py to reflect new modules.

---
+++ rave/__init__.py
@@ -0,0 +1,11 @@
+# rave - a modular and extensible visual novel engine.
+from . import log
+from . import bootstrap, bootstrappers
+from . import filesystem
+from . import loader
+
+__name__ = 'rave'
+__version__ = '0.1.0'
+__version_info__ = (0, 1, 0)
+__author__ = 'rave developers and contributors'
+__license__ = 'BSD'
