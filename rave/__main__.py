fc1cb951991cc9b4b0cdb9d533127d26eb21ea57
Shiz <hi@shiz.me>
2014-07-08 12:22:05 +0200

    rave: Remove -d command line argument in favour of __debug__.

--- rave/__main__.py
+++ rave/__main__.py
@@ -7,7 +7,6 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='A modular and extensible visual novel engine.', prog='rave')
     parser.add_argument('-b', '--bootstrapper', help='Select bootstrapper to bootstrap the engine with. (default: autoselect)')
     parser.add_argument('-B', '--game-bootstrapper', metavar='BOOTSTRAPPER', help='Select bootstrapper to bootstrap the game with. (default: autoselect)')
-    parser.add_argument('-d', '--debug', action='store_true', help='Enable debug logging.')
     parser.add_argument('game', metavar='GAME', nargs='?', help='The game to run. Format dependent on used bootstrapper.')
 
     arguments = parser.parse_args()
@@ -15,10 +14,6 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-
-    if args.debug:
-        from . import log
-        log.Logger.LEVEL |= log.DEBUG
 
     from . import bootstrap
     bootstrap.bootstrap_engine(args.bootstrapper)
