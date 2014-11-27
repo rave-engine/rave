56fecca1dd5edbd05b94ebd1ef1a8b74544e5bce
Shiz <hi@shiz.me>
2014-11-27 16:43:07 +0100

    rave: Rewrite module loader for new Python 3.4 import semantics.

--- rave/loader.py
+++ rave/loader.py
@@ -4,24 +4,14 @@ rave module loader.
 This module allows code to install Python import hooks in order to load Python modules from the virtual file system.
 These hooks will themselves hook Python's import code to search certain given paths in the VFS for modules in a certain package.
 """
-import os
 import sys
-import imp
 import importlib.abc
 import importlib.machinery
 import importlib.util
-import threading
-import marshal
 
 import rave.log
 import rave.filesystem
 import rave.game
-
-
-## Constants.
-
-SOURCE_ENCODING = 'utf-8'
-SOURCE_FALLBACK_ENCODING = 'iso-8859-1'
 
 
 ## Internals.
@@ -32,157 +22,6 @@ _log = rave.log.get(__name__)
 
 ## Loader classes.
 
-class VFSModuleLoader(importlib.abc.InspectLoader):
-    """ A loader that loads modules from a virtual file system. """
-
-    def __init__(self, filesystem):
-        self._modules = {}
-        self._filesystem = filesystem
-
-    def register(self, name, parent, path, is_package):
-        """ Register module as loadable through this loader. """
-        self._modules[name] = {
-            'path': path,
-            'parent': parent,
-            'handle': self._filesystem.open(path),
-            'is_package': is_package,
-            'source': None,
-            'code': None
-        }
-
-    def _load_code(self, name):
-        """ Load (and optionally compile) module code. """
-        data = self._modules[name]
-
-        # Determine how to load code. Do we load from a precompiled Python source?
-        if any(data['path'].endswith(ext) for ext in importlib.machinery.BYTECODE_SUFFIXES):
-            data['source'] = None
-            data['code'] = self._load_compiled(data['handle'])
-            _log.debug('Loaded module {name} from bytecode.', name=name)
-        # Or do we load from raw Python source?
-        elif any(data['path'].endswith(ext) for ext in importlib.machinery.SOURCE_SUFFIXES):
-            data['source'], data['code'] = self._load_source(data['handle'], data['path'])
-            _log.debug('Loaded module {name} from source.', name=name)
-        else:
-            raise ImportError('Unsure how to load code from file "{}".'.format(data['path']))
-
-    def _load_compiled(self, handle):
-        """ Load code object from compiled file. """
-        # .pyc files are very simple: a magic number, a timestamp and marshaled code.
-        with handle as f:
-            magic = f.read(4)
-            time = f.read(4)
-            marshaled = f.read()
-
-        # Sanity check.
-        if magic != imp.get_magic():
-            raise ImportError('This file has been compiled for a different Python version than currently running.')
-
-        # Unmarshal that code.
-        return marshal.loads(marshaled)
-
-    def _load_source(self, handle, path):
-        """ Load code object from source. """
-        # Read code.
-        with handle as f:
-            source = f.read()
-
-        # Decode code.
-        source = self._decode_source(source)
-        # Compile code!
-        return source, self.source_to_code(source, path)
-
-    def _decode_source(self, source):
-        """ Decode binary source to text. """
-        # Check if the easier Python 3.4+ API is available first.
-        if hasattr(importlib.util, 'decode_source'):
-            return importlib.util.decode_source(source)
-
-        # Do the manual way.
-        try:
-            txt = source.decode(SOURCE_ENCODING)
-        except UnicodeDecodeError:
-            txt = source.decode(SOURCE_FALLBACK_ENCODING)
-
-        # Windows-style newlines.
-        txt = txt.replace('\r\n', '\n')
-        # Mac OS9-style newlines.
-        txt = txt.replace('\r', '\n')
-        # Native newlines.
-        txt = txt.replace(os.linesep, '\n')
-        return txt
-
-    @importlib.util.module_for_loader
-    def load_module(self, module):
-        """ Load module. Python 3.3- API for module loading. """
-        name = module.__name__
-        if name not in self._modules:
-            raise ImportError('Incorrect module name for this loader.')
-        info = self._modules[name]
-
-        # Set relevant metadata.
-        module.__file__ = info['path']
-        if info['is_package']:
-            module.__path__ = []
-
-        self.exec_module(module)
-        return module
-
-    def exec_module(self, module):
-        """ Execute module code. Python 3.4+ API for module loading. """
-        name = module.__name__
-        if name not in self._modules:
-            raise ImportError('Incorrect module name for this loader.')
-        info = self._modules[name]
-
-        # Only log loading of top-level modules.
-        cleaned_name = name.replace(info['parent'], '').strip('.')
-        if '.' not in cleaned_name:
-            _log('Loading module {name}...', name=name)
-        else:
-            _log.debug('Loading submodule {name}...', name=name)
-
-        if info['code'] is None:
-            self._load_code(name)
-
-        # TODO: Implement execution environments and make this use them.
-        exec(info['code'], module.__dict__)
-
-    def is_package(self, name):
-        """ Determine whether or not the given module is a package. """
-        if name not in self._modules:
-            raise ImportError('Incorrect module name for this loader.')
-        return self._modules[name]['is_package']
-
-    def get_source(self, name):
-        """ Get module source code. """
-        if name not in self._modules:
-            raise ImportError('Incorrect module name for this loader.')
-
-        if self._modules[name]['code'] is None:
-            self._load_code(name)
-        return self._modules[name]['source']
-
-    def get_code(self, name):
-        """ Get module code. """
-        if name not in self._modules:
-            raise ImportError('Incorrect module name for this loader.')
-
-        if self._modules[name]['code'] is None:
-            self._load_code(name)
-        return self._modules[name]['code']
-
-    def source_to_code(self, source, path):
-        """ Compile source code to code. Python 3.4+ API. """
-        return compile(source, path, 'exec', dont_inherit=True)
-
-    def module_repr(self, module):
-        """ Get representation of module. Python 3.3- API. """
-        if module.__name__ not in self._modules:
-            raise ImportError('Incorrect module name for this loader.')
-        return "<rave module '{}' from '{}'>".format(module.__name__, self._modules[module.__name__]['path'])
-
-
 class EmptyPackageLoader(importlib.abc.InspectLoader):
     """ A loader that creates empty package modules. """
 
@@ -192,15 +31,6 @@ class EmptyPackageLoader(importlib.abc.InspectLoader):
     def register(self, package):
         """ Register package as loadable through this loader. """
         self._packages.add(package)
-
-    @importlib.util.module_for_loader
-    def load_module(self, module):
-        """ Load module code. Python 3.3- API for module loading. """
-        if module.__name__ not in self._packages:
-            raise ImportError('Incorrect module name for this loader.')
-
-        self.exec_module(module)
-        return module
 
     def exec_module(self, module):
         """ Execute module code. Python 3.4+ API for module loading. """
@@ -227,89 +57,118 @@ class EmptyPackageLoader(importlib.abc.InspectLoader):
             raise ImportError('Incorrect module name for this loader.')
         return None
 
-    def module_repr(self, module):
-        """ Get representation of module. Python 3.3- API. """
-        if module.__name__ not in self._packages:
-            raise ImportError('Incorrect module name for this loader.')
-        return "<rave module holder '{}'>".format(module.__name__)
 
-
-class VFSModuleFinder(importlib.abc.MetaPathFinder):
-    """ A module that attempts to find modules in the virtual file system and pass them to relevant loaders. """
-    _loaders = {}
+class VFSImporter(importlib.abc.MetaPathFinder, importlib.abc.SourceLoader):
+    """ A module that attempts to find modules in the virtual file system and loads them. """
     _package_loader = EmptyPackageLoader()
 
     def __init__(self, search_paths, package):
         self.search_paths = search_paths
         self.package = package
         self._package_loader.register(package)
+        self._modules = {}
 
-    def find_module(self, name, path=None):
-        """ Find loader for given module. Python 3.3- API. """
+    def __repr__(self):
+        return '<{}.{}: {}.*>'.format(self.__module__, self.__class__.__name__, self.package)
+
+    def find_spec(self, name, path, target=None):
+        """ Find ModuleSpec for given module. Attempt to see if module exists, basically. """
         # We only find modules intended for us.
         if name != self.package and not name.startswith(self.package + '.'):
             return None
 
-        # Create empty 'holder' module.
         if name == self.package:
-            return self._package_loader
+            # Use empty package holder module.
+            loader = self._package_loader
+            origin = None
+            candidates = []
+        else:
+            # Do we have a file system to load from?
+            current = rave.game.current()
+            if not current:
+                return None
 
-        # Do we have a file system to load from?
+            # Make relative path and find module.
+            rel_path = name.replace(self.package + '.', '').replace('.', current.fs.PATH_SEPARATOR)
+            _log.debug('Searching for {pkg}...', pkg=name)
+
+            for search_path in self.search_paths:
+                base_name = current.fs.join(search_path, rel_path)
+                extensions = importlib.machinery.SOURCE_SUFFIXES + importlib.machinery.BYTECODE_SUFFIXES
+                available = []
+
+                # Single-file modules.
+                candidates = [ base_name + ext for ext in extensions ]
+                available.extend(path for path in candidates if current.fs.isfile(path))
+                # Packages.
+                candidates = [ current.fs.join(base_name, '__init__' + ext) for ext in extensions ]
+                available.extend(path for path in candidates if current.fs.isfile(path))
+
+                # Find first available candidate.
+                for path in available:
+                    self._register_module(current, name, path)
+
+                    loader = self
+                    origin = path
+                    candidates = available
+                    break
+                else:
+                    # Nothing found.
+                    return None
+
+        if origin:
+            _log.debug('Loading {pkg} from {path}. (candidates: {available})', pkg=name, path=origin, available=candidates)
+        return importlib.machinery.ModuleSpec(name, loader, origin=origin)
+
+    def _register_module(self, current, name, path):
+        """ Register module as loadable through this loader. """
+        self._modules.setdefault(current, {})
+        self._modules[current][name] = path
+
+    def get_filename(self, name):
+        current = rave.game.current()
+        if not current or current not in self._modules or name not in self._modules[current]:
+            raise ImportError('Unknown module for this loader.')
+        return self._modules[current][name]
+
+    def get_data(self, path):
         current = rave.game.current()
         if not current:
-            return None
-        if current not in self._loaders:
-            self._loaders[current] = VFSModuleLoader(current.fs)
+            raise BrokenPipeError('No game file system to load module from.')
+        with current.fs.open(path, 'rb') as f:
+            return f.read()
 
-        # Search module path in the virtual file system.
-        relpath = name.replace(self.package + '.', '').replace('.', current.fs.PATH_SEPARATOR)
-        _log.debug('Attempting to load {pkg} from VFS..', pkg=name)
+    def set_data(self, path, data):
+        current = rave.game.current()
+        if not current:
+            # Silence errors.
+            return
 
-        for searchpath in self.search_paths:
-            basepath = current.fs.join(searchpath, relpath)
-            existing = []
-            extensions = importlib.machinery.SOURCE_SUFFIXES + importlib.machinery.BYTECODE_SUFFIXES
+        try:
+            with current.fs.open(path, 'wb') as f:
+                f.write(data)
+        except rave.filesystem.FileSystemError:
+            # Silence errors.
+            pass
 
-            # Attempt single-file modules first.
-            modpaths = [ basepath + ext for ext in extensions ]
-            existing.extend((path, False) for path in modpaths if current.fs.isfile(path))
-            # Attempt packages.
-            packagepaths = [ current.fs.join(basepath, '__init__' + ext) for ext in extensions ]
-            existing.extend((path, True) for path in packagepaths if current.fs.isfile(path))
+    def path_stats(self, path):
+        current = rave.game.current()
+        if not current:
+            raise BrokenPipeError('No game file system to get module info from.')
+        if not current.fs.isfile(path):
+            raise FileNotFoundError(path)
 
-            # Get the first one we can load.
-            for path, is_package in existing:
-                try:
-                    self._loaders[current].register(name, self.package, path, is_package)
-                    _log.debug('Found {pkg} in VFS: {path}. (candidates: {existing})', pkg=name, path=path, existing=[file for file, _ in existing])
-                    return self._loaders[current]
-                except rave.filesystem.FileNotFound:
-                    continue
-
-        # Nothing found.
-        _log.debug('{pkg} not found in VFS.', pkg=name)
-        return None
-
-    def find_spec(self, name, path, target=None):
-        """ Find ModuleSpec for given module. Python 3.4+ API. """
-        loader = self.find_module(name, path)
-        if not loader:
-            return None
-
-        return importlib.util.spec_from_loader(name, loader)
-
-    def __repr__(self):
-        return '<rave.loader.ModuleFinder ({})>'.format(self.package)
+        return { 'mtime': 0 }
 
 
 ## API.
 
-def install_hook(package, paths):
+def install_hook(package, paths, cls=VFSImporter):
     """
     Register an import hook for the virtual file system. Returns an identifier that can be passed to `remove_hook`.
     `package` gives the base package this hook should apply to, `paths` the search paths in the VFS the hook should search in.
     """
-    finder = VFSModuleFinder(paths, package)
+    finder = cls(paths, package)
     sys.meta_path.insert(0, finder)
     _installed_finders.append(finder)
 
