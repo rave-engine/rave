91aa325c8bfa69179cc9c2087e563fa8f9663919
Shiz <hi@shiz.me>
2015-03-14 04:35:32 +0100

    Rework module dependency system.

--- rave/modularity.py
+++ rave/modularity.py
@@ -11,8 +11,11 @@ PRIORITY_NEUTRAL = 0
 
 ## Internals.
 
+_loading_stack = []
+_requirements = {}
 _provisions = {}
 _provided = set()
+_available = {}
 _initialized = set()
 
 
@@ -23,18 +26,41 @@ class ModuleLoader(rave.loader.VFSImporter):
     A loader that loads rave engine modules, taking module initialization, requirements and provisions into account.
     """
     def exec_module(self, module):
-        super().exec_module(module)
+        # Register that we're initializing an engine module.
+        _loading_stack.append(module)
+        try:
+            super().exec_module(module)
+        finally:
+            _loading_stack.pop()
+
+        if _loading_stack:
+            # Another engine module tried to this module. Add it as dependency.
+            loader = _loading_stack[-1].__name__
+            _requirements.setdefault(loader, [])
+            _requirements[loader].append(module.__name__)
+
         register_module(module)
 
 
-def register_module(mod):
-    # Record provisions.
-    if not hasattr(mod, '__priority__'):
-        mod.__priority__ = PRIORITY_NEUTRAL
-    if hasattr(mod, '__provides__'):
-        for provision in mod.__provides__:
+def register_module(module):
+    if not hasattr(module, '__priority__'):
+        module.__priority__ = PRIORITY_NEUTRAL
+
+    # Store module.
+    _available[module.__name__] = module
+
+    # Register provisions.
+    if hasattr(module, '__provides__'):
+        for provision in module.__provides__:
             _provisions.setdefault(provision, [])
-            heapq.heappush(_provisions[provision], (mod.__priority__, id(mod), mod))
+            # Store the id as a 'random number' to make the tuple comparison for the heap queue work
+            # because modules objects are not comparable in case of identical priorities.
+            heapq.heappush(_provisions[provision], (module.__priority__, id(module), module))
+
+    # Register requirements.
+    if hasattr(module, '__requires__'):
+        _requirements.setdefault(module.__name__, [])
+        _requirements[module.__name__].extend(module.__requires__)
 
 def load_module(mod):
     for dependency in reversed(_resolve_dependencies(mod)):
@@ -56,12 +82,11 @@ def _resolve_dependencies(mod, resolving=None):
     if resolving is None:
         resolving = []
 
-    for requirement in getattr(mod, '__requires__', []):
+    for requirement in _requirements.get(mod.__name__, []):
         if requirement in resolving or _is_provided(requirement):
             continue
 
-        resolving.append(requirement)
-        for priority, _, provider in _provision_candidates(requirement):
+        for _, _, provider in _provision_candidates(requirement):
             old_resolving = resolving[:]
             try:
                 subdependencies = _resolve_dependencies(provider, resolving)
@@ -93,4 +118,6 @@ def _mark_provided(provision):
     _provided.add(provision)
 
 def _provision_candidates(provision):
+    if provision in _available:
+        return [provision] + _provisions.get(provision, [])
     return _provisions.get(provision, [])
