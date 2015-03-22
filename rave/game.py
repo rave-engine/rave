7d5d4fa735fa5a2d07fbeaa36e50a59b7be29927
Shiz <hi@shiz.me>
2015-03-22 11:38:44 +0100

    rave: Add game suspend/resume basics.

--- rave/game.py
+++ rave/game.py
@@ -3,6 +3,8 @@ rave game module.
 
 This contains the code that ties everything together to run a single game.
 """
+import threading
+import rave.log
 import rave.filesystem
 import rave.execution
 import rave.events
@@ -26,16 +28,33 @@ class Game:
     def __init__(self, name, base=None):
         self.name = name
         self.base = base
+        self.active_lock = threading.Lock()
         self.fs = rave.filesystem.FileSystem()
         self.events = rave.events.EventBus()
         self.env = rave.execution.ExecutionEnvironment(self)
         self.window = None
         self.mixer = None
 
+        self.events.hook('game.suspend', self.suspend)
+        self.events.hook('game.resume', self.resume)
+
         # Announce game creation over parent event bus.
         parent = current()
         if parent:
             parent.events.emit('game.created', self)
 
+    def suspend(self, event):
+        _log('Game suspending, acquiring main loop lock.')
+        self.active_lock.acquire()
+
+    def resume(self, event):
+        _log('Game resuming, releasing main loop lock.')
+        self.active_lock.release()
+
     def __repr__(self):
         return '<{}: {}>'.format(self.__class__.__qualname__, self.name)
+
+
+## Internals.
+
+_log = rave.log.get(__name__)
